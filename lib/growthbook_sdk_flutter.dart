2896101c7ce90f2853226f2141d71247886ab34f
Michael Denny <micdenny@gmail.com>
2025-04-15 17:52:45 +0200

    Export sticky_bucket_service.dart to library

--- lib/growthbook_sdk_flutter.dart
+++ lib/growthbook_sdk_flutter.dart
@@ -11,3 +11,4 @@ export 'src/Model/model.dart';
 export 'src/Network/network.dart';
 export 'src/Utils/utils.dart';
 export 'src/growth_book_sdk.dart';
+export 'src/StickyBucketService/sticky_bucket_service.dart';
