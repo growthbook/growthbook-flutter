b5e586d1c01296ae4d9eec0b11ee74184399a6ab
Madhu Chavva <madhu.chavva@gmail.com>
2025-06-18 12:29:45 -0700

    fix analyzer issues

--- lib/src/Utils/wrapper.dart
+++ lib/src/Utils/wrapper.dart
@@ -2,6 +2,8 @@ import 'package:growthbook_sdk_flutter/src/Utils/converter.dart';
 import 'package:json_annotation/json_annotation.dart';
 import 'package:tuple/tuple.dart';
 
+part 'wrapper.g.dart';
+
 @Tuple2Converter()
 @JsonSerializable()
 class Tuple2Wrapper {
