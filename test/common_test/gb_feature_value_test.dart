a3865ff5cb0ed2193d7390140c89299c2696fe7c
Volodymyr Nazarkevych <volodymyr.nazarkevych@kevychsoulutions.com>
2025-08-25 17:53:12 +0300

    fix feature synchronization on refresh

--- test/common_test/gb_feature_value_test.dart
+++ test/common_test/gb_feature_value_test.dart
@@ -85,7 +85,7 @@ void main() {
 
       for (final item in evaluateCondition) {
         if (item is List<dynamic>) {
-          sdk.feature(item[2]);
+          await sdk.feature(item[2]);
           break;
         }
       }
--- test/common_test/sdk_builder_test.dart
+++ test/common_test/sdk_builder_test.dart
@@ -87,9 +87,9 @@ void main() {
         growthBookTrackingCallBack: (trackData) {},
         backgroundSync: false,
       ).setRefreshHandler((refreshHandler) => refreshHandler = isRefreshed).initialize();
-      final featureValue = sdk.feature('fwrfewrfe');
+      final featureValue = await sdk.feature('some-feature');
       expect(featureValue.source, GBFeatureSource.unknownFeature);
-      final result = sdk.run(GBExperiment(key: "fwrfewrfe"));
+      final result = sdk.run(GBExperiment(key: "some-feature"));
       expect(result.variationID, 0);
       manager.clearCache();
     });
@@ -106,7 +106,7 @@ void main() {
           gbFeatures: {'some-feature': GBFeature(defaultValue: true)},
           backgroundSync: false,
         ).setRefreshHandler((refreshHandler) {}).initialize();
-        final featureValue = sdk.feature('some-feature');
+        final featureValue = await sdk.feature('some-feature');
         expect(featureValue.value ?? true, true);
         final result = sdk.run(GBExperiment(key: "some-feature"));
         expect(result.variationID, 0);
